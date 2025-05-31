8d2bcc643e7f6419b1bfc289a7f57e77459db452
DDISB <disbden@gmail.com>
2025-05-31 09:20:17 +0300

    -

---
+++ src/lib/main/mainScreen.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<div class="main-container">
+    f
+</div>
+
+<style>
+    .main-container {
+        width: 100vh;
+        height: 100ch;
+        background-color: white;
+    }
+</style>
