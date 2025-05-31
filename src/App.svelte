af8fc8decc52dfe79734fc367e4c50deb5704765
DDISB <disbden@gmail.com>
2025-05-31 09:22:20 +0300

    -

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import Auth from './lib/auth/auth.svelte';
   import MainScreen from './lib/main/mainScreen.svelte';
+  import Admin from './lib/admin/admin.svelte';
 </script>
 
 <main>
-  <Auth/>
+  <!-- <Auth/> -->
   <!-- <MainScreen /> -->
+  <Admin />
 </main>
 
 <style>
