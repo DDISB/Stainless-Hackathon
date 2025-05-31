2f05a755fd70bf0941ca3eaba3cbd7eba414e39b
DDISB <disbden@gmail.com>
2025-05-31 14:18:02 +0300

    -

---
+++ src/lib/pages/Shop.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+</script>
+
+<div class="main-container">
+    <div class="header">
+        <h1>Магазин</h1>
+    </div>
+
+    <div class="content">
+        <div class="content-block">
+            <!-- Shop content will go here -->
+        </div>
+    </div>
+</div>
+
+<style>
+    .main-container {
+        max-width: 1000px;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    .header {
+        width: 100%;
+        height: 50px;
+        display: flex;
+        align-items: center;
+    }
+
+    h1 {
+        font-size: 32px;
+        font-weight: 600;
+        margin: 0;
+    }
+
+    .content {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    .content-block {
+        width: 100%;
+        min-height: 50px;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    }
+</style> 
